a3bd9de0b35ce9fc008ceca09a7e0ddc7d8d08a4
Hwan0518 <qip0518@naver.com>
2024-07-30 12:25:55 +0900

    Initial commit

---
+++ src/main/java/kdh/mysamplecode/MySampleCodeApplication.java
@@ -0,0 +1,15 @@
+package kdh.mysamplecode;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MySampleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySampleCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kdh/mysamplecode/MySampleCodeApplicationTests.java
@@ -0,0 +1,15 @@
+package kdh.mysamplecode;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MySampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
